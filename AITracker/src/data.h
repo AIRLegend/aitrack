4c4e76350e46a663407189c026aefb06c1f347a6
AIRLegend <alvaroibrain@hotmail.com>
2020-07-19 16:58:26 +0200

    Initial

---
+++ AITracker/src/data.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+
+
+class FaceData
+{
+public:
+	// Pitch, Yaw, Roll
+	double* rotation;
+
+	// X, Y, Z
+	double* translation;
+
+	// Landmark positions: [[x,y], [x,y], [x,y], ...]
+	float landmark_coords[66 * 2]; 
+
+	// Crop position x0, y0, x1, y1
+	int* face_coords;
+
+	bool face_detected;
+
+	FaceData();
+
+	std::string to_string();
+};
+
