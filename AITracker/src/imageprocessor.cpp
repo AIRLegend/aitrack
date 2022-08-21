ac0958ab3b2c372aef523746ce03fe7da447c3ba
searching46dof <107732149+searching46dof@users.noreply.github.com>
2022-08-20 23:37:30 -0400

    Revert "Add ImageProcessor::normalize_and_transpose_and_filter to also filter image with a configurable center weighted filter"
    This reverts commit 184857b9b8b08b7eb89a02639c59103ff9f46838.

--- AITracker/src/imageprocessor.cpp
+++ AITracker/src/imageprocessor.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include "imageprocessor.h"
 
 
@@ -9,8 +8,7 @@ ImageProcessor::ImageProcessor() :
 	//mean_scaling = mean_scaling / std_scaling;
 	cv::divide(mean_scaling, std_scaling, mean_scaling);
 	std_scaling *= 255.0f;
-
- }
+}
 
 void ImageProcessor::normalize(cv::Mat& image)
 {
@@ -115,151 +113,4 @@ void ImageProcessor::normalize_and_transpose(cv::Mat& image, float* dest, int di
         }
     }
 }
-
-void ImageProcessor::normalize_and_transpose_and_filter(cv::Mat& image, float* dest, std::vector<float>& scalingFilter, int dim_x, int dim_y)
-{
-    if (scalingFilter.size() == 0)
-        return normalize_and_transpose(image, dest, dim_x, dim_y);
-
-    const int stride = dim_x * dim_y;
-
-    // combine normalize and transpose methods to reduce for loops
-    float* from = (float*)image.data;
-    for (int channel = 0; channel < 3; channel++)
-    {
-        float std_scaline_for_channel = (float)std_scaling[channel];
-        float mean_scaling_for_channel = (float)mean_scaling[channel];
-
-        for (int i = 0; i < stride; i++)
-        {
-            float& from_reference = from[channel + i * 3]; // use a reference to reduce indexing
-            from_reference /= std_scaline_for_channel; /* remove internal for for loop of cv::divide(image, std_scaling, image); */
-            from_reference -= mean_scaling_for_channel; /* remove internal for for loop of cv::subtract(image, mean_scaling, image); */
-
-            from_reference *= scalingFilter[ i ]; // scale according to initialized filter
-
-            dest[stride * channel + i] = from_reference; /* transpose */
-        }
-    }
-}
-
-
-float ImageProcessor::getDistance(int x1, int y1, int x2, int y2)
-{
-    int x_distance = (x1 - x2);
-    int y_distance = (y1 - y2);
-    float distance = (float)sqrt((x_distance * x_distance) + (y_distance * y_distance));
-    return distance;
-}
-
-
-std::vector<float>::size_type ImageProcessor::initFilter_conical(int dim_x, int dim_y)
-{
-    // create a conical filter
-    // 1.0 at center of rectangle dimensions dim_x * dim_y
-    // 0.5 at edges of x and y axis through the center
-    // provides center weight and linear rolloff towards edges
-
-    int x_center = dim_x / 2;
-    int y_center = dim_y / 2;
-
-    float scale_distance;
-    if (x_center >= y_center)
-        scale_distance = (float)x_center;
-    else
-        scale_distance = (float)y_center;
-
-    scalingFilter.resize(dim_y * dim_x);
-    for (int y = 0; y < dim_y; y++)
-    {
-        for (int x = 0; x < dim_x; x++)
-        {
-            float distance = getDistance(x, y, x_center, y_center);
-            float scale    = 1.0f - (0.5f * distance / scale_distance);
-
-            scalingFilter[y * dim_x + x] = scale;
-            //std::cout << x << "," << y << "," << scale << std::endl;
-        }
-    }
-
-    return scalingFilter.size();
-}
-
-
-std::vector<float>::size_type ImageProcessor::initFilter_vignette(int dim_x, int dim_y)
-{
-    // create a vignette filter with a normal(gaussian) distribution
-    // 1.0 at center of rectangle dimensions dim_x * dim_y
-    // apprx 0.34 at edges of x and y axis through the center
-    // provides center weight and gaussian rolloff towards edges
-
-    int x_center = dim_x / 2;
-    int y_center = dim_y / 2;
-
-    float scale_distance;
-    if (x_center >= y_center)
-        scale_distance = (float)x_center;
-    else
-        scale_distance = (float)y_center;
-    float radius = 1.0f;
-    float maxImageRadius = radius * getDistance(dim_x, dim_y, x_center, y_center);
-  
-    scalingFilter.resize(dim_y * dim_x);
-    for (int y = 0; y < dim_y; y++)
-    {
-        for (int x = 0; x < dim_x; x++)
-        {
-            float distance    = getDistance(x, y, x_center, y_center);
-            float radiusRatio = distance / maxImageRadius;
-            float scale       = (float)pow( cos(radiusRatio), 4);
-
-            scalingFilter[y * dim_x + x] = scale;
-            //std::cout << x << "," << y << "," << scale << std::endl;
-        }
-    }
-
-    return scalingFilter.size();
-}
-
-
-std::vector<float>::size_type ImageProcessor::initFilter_digitalZoom(int dim_x, int dim_y, int zoom)
-{
-    // create a digital zoom filter
-    // 1.0 within centered rectangle dimensions (dim_x/zoom) * (dim_y/zoom)
-    // 0.0 outside of centerd rectangle
-    // provides level center weight within zoom factor rectangle and masks surrouding borders
-
-    int x_center = dim_x / 2;
-    int y_center = dim_y / 2;
-
-    if (zoom < 1)
-        zoom = 1;
-    else if (zoom > 4)
-        zoom = 4;
-    int x_left   = x_center - (x_center / zoom);
-    int x_right  = x_center + (x_center / zoom);
-    int y_top    = y_center - (y_center / zoom);
-    int y_bottom = y_center + (y_center / zoom);
-
-    scalingFilter.resize(dim_y * dim_x);
-    for (int y = 0; y < dim_y; y++)
-    {
-        for (int x = 0; x < dim_x; x++)
-        {
-            float scale;
-            if ((y < y_top)  || (y >= y_bottom) ||
-                (x < x_left) || (x >= x_right))
-                scale = 0.0f;
-            else
-                scale = 1.0f;
-
-            scalingFilter[ y * dim_x + x ] = scale;
-            //std::cout << x << "," << y << "," << scale << std::endl;
-        }
-    }
-
-    return scalingFilter.size();
-}
-
-
 #endif
--- AITracker/src/imageprocessor.h
+++ AITracker/src/imageprocessor.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <vector>
 #include "opencv.hpp"
 
 #define OPTIMIZE_ImageProcessor 1
@@ -14,12 +13,6 @@ public:
 	void transpose(float* from, float* dest, int dim_x = 224, int dim_y = 224);
 #ifdef OPTIMIZE_ImageProcessor
 	void normalize_and_transpose(cv::Mat& image, float* dest, int dim_x = 224, int dim_y = 224);
-	void normalize_and_transpose_and_filter(cv::Mat& image, float* dest, std::vector<float>& scalingFilter, int dim_x = 224, int dim_y = 224);
-	float getDistance(int x, int y, int x_center, int y_center);
-	std::vector<float>::size_type initFilter_vignette(int dim_x = 224, int dim_y = 224);
-	std::vector<float>::size_type initFilter_conical(int dim_x = 224, int dim_y = 224);
-	std::vector<float>::size_type initFilter_digitalZoom(int dim_x = 224, int dim_y = 224, int zoom = 2);
-	std::vector<float> scalingFilter;
 #endif
 
 private:
